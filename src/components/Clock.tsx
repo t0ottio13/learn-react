2ef2b11be6f65307aec2d1cfbdc49d7b084c8873
h455h1 <adsoption104@gmail.com>
2022-11-06 17:36:02 +0900

    [modify] useLayoutEffectを使用し、コンポーネントの関数を変更

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 
 const UPDATE_CYCLE = 1000;
 
@@ -40,24 +40,35 @@ export const Clock = () => {
         // 第二引数に空配列を渡すと初期描画時のみに実行される
     }, [])
 
+    // React.StrictModeの影響で２回実行されることへの対処
+    const mounted = React.useRef(false);
+
     // localStorageの読み込み
-    useEffect(() => {
-        const saveLocale = localStorage.getItem(KEY_LOCAL)
-        if(saveLocale !== null){
-            setLocale(getLocalFromString(saveLocale));
+    useLayoutEffect(() => {
+        // すでに一回実行されていれば、スキップ
+        if(mounted.current){
+            return
+        }
+        mounted.current = true;
+
+        const savedLocale = localStorage.getItem(KEY_LOCAL)
+        if(savedLocale !== null){
+            setLocale(getLocalFromString(savedLocale));
+            console.log(`${savedLocale}を読み込み`);
         }
     }, [])
 
     // localStorageへの保存
     useEffect(() => {
         localStorage.setItem(KEY_LOCAL, locale);
+        console.log(`${locale}を保存`)
     }, [locale])
 
     return (
         <div>
             <p>
                 <span id="current-time-label">現在時刻</span>
-                <span>:{timestamp.toLocaleTimeString(locale)}</span>
+                <span>:{timestamp.toLocaleString(locale)}</span>
                 <select
                     value={locale}
                     onChange={(e) => setLocale(getLocalFromString(e.target.value))}
@@ -70,10 +81,13 @@ export const Clock = () => {
     )
 
     // まとメモ
+    //
     // useEffectは、描画関数を実行し、DOMが更新され、画面に描画された後で実行される
     // これの仕組みにより、再描画の無限ループを防げる
     // 例
     // 1.ユーザー操作で状態が変わる
     // 2.描画関数が実行され、状態を更新して、再描画
     // 3.状態の更新をキャッチして、また描画関数が.....無限ループ
+    //
+    // useLayoutEffectは、DOM更新後、画面描画前に実行される
 }
